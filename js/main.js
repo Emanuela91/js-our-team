a3d21bbaad2bb36fa4e054f3ff8ab90ad78fba36
Emanuela91 <emanuela.marson91@gmail.com>
2022-11-21 16:08:44 +0100

    creato html, css e js. Js fatto fino a milestone 1: creato array di oggetti e stampati in console

---
+++ js/main.js
@@ -0,0 +1,57 @@
+// Utilizzando i dati forniti, creare un array di oggetti per rappresentare i membri del team.
+// Ogni membro è caratterizzato dalle seguenti informazioni: nome, ruolo e foto.
+// MILESTONE 0:
+// Creare l’array di oggetti con le informazioni fornite.
+
+const membriTeam = [
+    {
+        "nome" : "Wayne Barnett",
+        "role" : "Founder e CEO",
+        "foto" : "wayne-barnett-founder-ceo.jpg"
+    },
+    {
+        "nome" : "Agela Caroll",
+        "role" : "Chief Editor",
+        "foto" : "angela-caroll-chief-editor.jpg" 
+    },
+    {
+        "nome" : "Walter Gordon",
+        "role" : "Office Manager",
+        "foto" : "walter-gordon-office-manager.jpg"
+    },
+    {
+        "nome" : "Angela Lopez",
+        "role" : "Social Media Manager",
+        "foto" : "angela-lopez-social-media-manager.jpg"
+    },
+    {
+        "nome" : "Scott Estrada",
+        "role" : "Developer",
+        "foto" : "scott-estrada-developer.jpg"
+    },
+    {
+        "nome" : "Barbara Ramos",
+        "role" : "Graphic Designer",
+        "foto" : "barbara-ramos-graphic-designer.jpg"
+    }
+];
+
+
+// MILESTONE 1:
+// Stampare su console, per ogni membro del team, le informazioni di nome, ruolo e la stringa della foto
+for (let i = 0; i < membriTeam.length; i++){
+    let Team = membriTeam[i];
+    console.log(Team);
+}
+
+
+// MILESTONE 2:
+// Stampare le stesse informazioni su DOM sottoforma di stringhe.
+
+
+
+// BONUS 1:
+// Trasformare la stringa foto in una immagine effettiva
+// BONUS 2:
+// Organizzare i singoli membri in card/schede.
+
